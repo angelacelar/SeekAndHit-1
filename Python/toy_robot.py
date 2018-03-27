26b897f337a18bd5dd0498ac5a3f3094e9127147
krunomunitic <krunomunitic@gmail.com>
2018-03-27 18:12:00 +0200

    FInish

--- Python/toy_robot.py
+++ Python/toy_robot.py
@@ -4,9 +4,7 @@ from collections import OrderedDict
 class InputException(Exception):
     pass
 
-
-class ToyRobot:
-    _position = {'direction': None, 'x': None, 'y': None}
+class TableTop(object):
     direction_values = OrderedDict([
         ('NORTH', {'x': 0, 'y': 1}),
         ('EAST', {'x': 1, 'y': 0}),
@@ -14,50 +12,75 @@ class ToyRobot:
         ('WEST', {'x': -1, 'y': 0})
     ])
 
-    _valid_range = range(0, 5)
+    valid_range = range(0, 5)
 
-    def set_position(self, x, y, direction):
-        self._position = {'direction': direction, 'x': x, 'y': y}
+
+class Position(object):
+    direction = None
+    x = None
+    y = None
+
+    def __init__(self, direction='EAST', x=0, y=0):
+        self.direction = direction
+        self.x = int(x)
+        self.y = int(y)
 
     def is_valid_position(self, x, y, direction):
-        if (int(x) in self._valid_range and int(y) in self._valid_range and
-                direction in self.direction_values):
-
+        if (int(x) in TableTop.valid_range and int(y) in TableTop.valid_range and
+                direction in TableTop.direction_values):
             return True
         else:
             return False
 
-    def move_robot(self):
-        direction_value = self.direction_values[self._position['direction']]
-        temp_x = self._position['x'] + direction_value['x']
-        temp_y = self._position['y'] + direction_value['y']
 
-        if temp_x in self._valid_range and temp_y in self._valid_range:
-            self._position['x'] = temp_x
-            self._position['y'] = temp_y
+class ToyRobot(object):
+    position = Position()
 
-    def calculate_rotation(self, direction, leap):
-        possible_directions = self.direction_values.keys()
+    def set_position(self, x, y, direction):
+        self.position.x = x
+        self.position.y = y
+        self.position.direction = direction
+
+    def change_position(self):
+        direction_value = TableTop.direction_values[self.position.direction]
+        temp_x = self.position.x + direction_value['x']
+        temp_y = self.position.y + direction_value['y']
+
+        if temp_x in TableTop.valid_range and temp_y in TableTop.valid_range:
+            self.position.x = temp_x
+            self.position.y = temp_y
+
+    def _calculate_rotation(self, direction, leap):
+        possible_directions = TableTop.direction_values.keys()
         return possible_directions[(possible_directions.index(direction) + leap + len(possible_directions)) % len(possible_directions)]
 
     def rotate_left(self):
-        self._position['direction'] = self.calculate_rotation(self._position['direction'], -1)
+        self.position.direction = self._calculate_rotation(self.position.direction, -1)
 
     def rotate_right(self):
-        self._position['direction'] = self.calculate_rotation(self._position['direction'], 1)
+        self.position.direction = self._calculate_rotation(self.position.direction, 1)
 
     def report(self):
-        print self._position['x'], self._position['y'], self._position['direction']
+        print self.position.x, self.position.y, self.position.direction
+
+    def set_initial_robot_position(self, positioning):
+        if (len(positioning) == 3 and positioning[0].isdigit() and positioning[1].isdigit() and
+                self.position.is_valid_position(positioning[0], positioning[1], positioning[2])):
+
+            self.set_position(int(positioning[0]), int(positioning[1]), positioning[2])
+
+            return True
+        return False
 
 
-def move_to_begining_of_commands(commands):
-    places_command = [index for index, c in enumerate(commands) if c == 'PLACE']
+def find_first_place_command(commands):
+        places_command = [index for index, c in enumerate(commands) if c == 'PLACE']
 
-    if len(places_command) and len(commands) > 1:
-        index_last_place_command = places_command[-1]
+        if len(places_command) and len(commands) > 1:
+            index_last_place_command = places_command[-1]
 
-        return commands[index_last_place_command + 1:]
-    return []
+            return commands[index_last_place_command + 1:]
+        return []
 
 
 def parse_commands(commands, toy_robot):
@@ -67,29 +90,19 @@ def parse_commands(commands, toy_robot):
         elif command == 'RIGHT':
             toy_robot.rotate_right()
         elif command == 'MOVE':
-            toy_robot.move_robot()
+            toy_robot.change_position()
         elif command == 'REPORT':
             toy_robot.report()
 
 
-def proper_positioning(positioning, toy_robot):
-    if (len(positioning) == 3 and positioning[0].isdigit() and positioning[1].isdigit() and
-            toy_robot.is_valid_position(positioning[0], positioning[1], positioning[2])):
-
-        toy_robot.set_position(int(positioning[0]), int(positioning[1]), positioning[2])
-
-        return True
-    return False
-
-
 def main(text):
     toy_robot = ToyRobot()
-    commands = move_to_begining_of_commands(text.split())
+    commands = find_first_place_command(text.split())
 
     if len(commands):
         positioning_command = commands[0].split(',')
 
-        if proper_positioning(positioning_command, toy_robot):
+        if toy_robot.set_initial_robot_position(positioning_command):
             parse_commands(commands[1:], toy_robot)
         else:
             raise InputException('Toy robot begin position command is not valid.')
--- Python/toy_robot_test.py
+++ Python/toy_robot_test.py
@@ -1,5 +1,5 @@
 import unittest
-from toy_robot import ToyRobot, move_to_begining_of_commands, parse_commands, proper_positioning
+from toy_robot import ToyRobot, Position, find_first_place_command, parse_commands
 
 
 class TestToyRobot(unittest.TestCase):
@@ -8,114 +8,119 @@ class TestToyRobot(unittest.TestCase):
         self.toy_robot.set_position(0, 0, 'EAST')
 
     def test_set_position(self):
-        self.assertEqual(self.toy_robot._position['x'], 0)
-        self.assertEqual(self.toy_robot._position['y'], 0)
-        self.assertEqual(self.toy_robot._position['direction'], 'EAST')
+        self.assertEqual(self.toy_robot.position.x, 0)
+        self.assertEqual(self.toy_robot.position.y, 0)
+        self.assertEqual(self.toy_robot.position.direction, 'EAST')
 
         self.toy_robot.set_position(1, 1, 'WEST')
-        self.assertEqual(self.toy_robot._position['x'], 1)
-        self.assertEqual(self.toy_robot._position['y'], 1)
-        self.assertEqual(self.toy_robot._position['direction'], 'WEST')
-
-    def test_is_valid_position(self):
-        is_valid = self.toy_robot.is_valid_position('1', '2', '2')
-        self.assertEqual(is_valid, False)
-
-        is_valid = self.toy_robot.is_valid_position('1', '1', 'test')
-        self.assertEqual(is_valid, False)
-
-        is_valid = self.toy_robot.is_valid_position('1', '1', 'EAST')
-        self.assertEqual(is_valid, True)
+        self.assertEqual(self.toy_robot.position.x, 1)
+        self.assertEqual(self.toy_robot.position.y, 1)
+        self.assertEqual(self.toy_robot.position.direction, 'WEST')
 
     def test_rotate_left(self):
         self.toy_robot.rotate_left()
-        self.assertEqual(self.toy_robot._position['direction'], 'NORTH')
+        self.assertEqual(self.toy_robot.position.direction, 'NORTH')
 
     def test_rotate_right(self):
         self.toy_robot.rotate_right()
-        self.assertEqual(self.toy_robot._position['direction'], 'SOUTH')
+        self.assertEqual(self.toy_robot.position.direction, 'SOUTH')
 
     def test_parse_commands(self):
         parse_commands((['test', 'TEST', 'move']), self.toy_robot)
-        self.assertEqual(self.toy_robot._position['x'], 0)
-        self.assertEqual(self.toy_robot._position['y'], 0)
-        self.assertEqual(self.toy_robot._position['direction'], 'EAST')
+        self.assertEqual(self.toy_robot.position.x, 0)
+        self.assertEqual(self.toy_robot.position.y, 0)
+        self.assertEqual(self.toy_robot.position.direction, 'EAST')
 
         parse_commands((['MOVE', 'MOVE', 'LEFT', 'MOVE', 'MOVE']), self.toy_robot)
-        self.assertEqual(self.toy_robot._position['x'], 2)
-        self.assertEqual(self.toy_robot._position['y'], 2)
-        self.assertEqual(self.toy_robot._position['direction'], 'NORTH')
+        self.assertEqual(self.toy_robot.position.x, 2)
+        self.assertEqual(self.toy_robot.position.y, 2)
+        self.assertEqual(self.toy_robot.position.direction, 'NORTH')
 
         self.toy_robot.set_position(0, 0, 'EAST')
         parse_commands((['MOVE', 'MOVE', 'RIGHT', 'MOVE', 'MOVE']), self.toy_robot)
-        self.assertEqual(self.toy_robot._position['x'], 2)
-        self.assertEqual(self.toy_robot._position['y'], 0)
-        self.assertEqual(self.toy_robot._position['direction'], 'SOUTH')
+        self.assertEqual(self.toy_robot.position.x, 2)
+        self.assertEqual(self.toy_robot.position.y, 0)
+        self.assertEqual(self.toy_robot.position.direction, 'SOUTH')
 
         # test movement to the edge
         self.toy_robot.set_position(0, 0, 'NORTH')
         parse_commands((['RIGHT', 'MOVE', 'MOVE', 'MOVE', 'MOVE', 'MOVE', 'MOVE']), self.toy_robot)
-        self.assertEqual(self.toy_robot._position['x'], 4)
-        self.assertEqual(self.toy_robot._position['y'], 0)
-        self.assertEqual(self.toy_robot._position['direction'], 'EAST')
+        self.assertEqual(self.toy_robot.position.x, 4)
+        self.assertEqual(self.toy_robot.position.y, 0)
+        self.assertEqual(self.toy_robot.position.direction, 'EAST')
 
         # test movement to the edge
         self.toy_robot.set_position(0, 0, 'NORTH')
         parse_commands((['MOVE', 'MOVE', 'MOVE', 'MOVE', 'MOVE', 'MOVE']), self.toy_robot)
-        self.assertEqual(self.toy_robot._position['x'], 0)
-        self.assertEqual(self.toy_robot._position['y'], 4)
-        self.assertEqual(self.toy_robot._position['direction'], 'NORTH')
+        self.assertEqual(self.toy_robot.position.x, 0)
+        self.assertEqual(self.toy_robot.position.y, 4)
+        self.assertEqual(self.toy_robot.position.direction, 'NORTH')
 
         # test movement over the edge if in 0 0
         self.toy_robot.set_position(0, 0, 'WEST')
         parse_commands((['LEFT', 'MOVE', 'MOVE', 'MOVE', 'MOVE']), self.toy_robot)
-        self.assertEqual(self.toy_robot._position['x'], 0)
-        self.assertEqual(self.toy_robot._position['y'], 0)
-        self.assertEqual(self.toy_robot._position['direction'], 'SOUTH')
+        self.assertEqual(self.toy_robot.position.x, 0)
+        self.assertEqual(self.toy_robot.position.y, 0)
+        self.assertEqual(self.toy_robot.position.direction, 'SOUTH')
 
         # test movement over the edge if in 0 0
         self.toy_robot.set_position(0, 0, 'EAST')
         parse_commands((['LEFT', 'LEFT', 'MOVE', 'MOVE', 'MOVE']), self.toy_robot)
-        self.assertEqual(self.toy_robot._position['x'], 0)
-        self.assertEqual(self.toy_robot._position['y'], 0)
-        self.assertEqual(self.toy_robot._position['direction'], 'WEST')
+        self.assertEqual(self.toy_robot.position.x, 0)
+        self.assertEqual(self.toy_robot.position.y, 0)
+        self.assertEqual(self.toy_robot.position.direction, 'WEST')
 
         # test movement over the edge if in 4 4
         self.toy_robot.set_position(4, 4, 'EAST')
         parse_commands((['MOVE', 'MOVE', 'MOVE', 'MOVE']), self.toy_robot)
-        self.assertEqual(self.toy_robot._position['x'], 4)
-        self.assertEqual(self.toy_robot._position['y'], 4)
-        self.assertEqual(self.toy_robot._position['direction'], 'EAST')
+        self.assertEqual(self.toy_robot.position.x, 4)
+        self.assertEqual(self.toy_robot.position.y, 4)
+        self.assertEqual(self.toy_robot.position.direction, 'EAST')
 
         # test movement over the edge if in 4 4
         self.toy_robot.set_position(4, 4, 'NORTH')
         parse_commands((['RIGHT', 'MOVE', 'MOVE', 'MOVE']), self.toy_robot)
-        self.assertEqual(self.toy_robot._position['x'], 4)
-        self.assertEqual(self.toy_robot._position['y'], 4)
-        self.assertEqual(self.toy_robot._position['direction'], 'EAST')
+        self.assertEqual(self.toy_robot.position.x, 4)
+        self.assertEqual(self.toy_robot.position.y, 4)
+        self.assertEqual(self.toy_robot.position.direction, 'EAST')
 
-    def test_move_to_begining_of_commands(self):
-        self.assertEqual(move_to_begining_of_commands(['PLACE']), [])
-        self.assertEqual(move_to_begining_of_commands(['PLACE', 'TEST']), ['TEST'])
-        self.assertEqual(move_to_begining_of_commands(
+    def test_find_first_place_command(self):
+        self.assertEqual(find_first_place_command(['PLACE']), [])
+        self.assertEqual(find_first_place_command(['PLACE', 'TEST']), ['TEST'])
+        self.assertEqual(find_first_place_command(
             ['PLACE', '1,2,EAST', 'PLACE', 'TEST']), ['TEST'])
 
-    def test_proper_positioning(self):
-        is_valid = proper_positioning(['TEST', '2'], self.toy_robot)
+    def test_set_initial_robot_position(self):
+        is_valid = self.toy_robot.set_initial_robot_position(['TEST', '2'])
         self.assertEqual(is_valid, False)
 
-        is_valid = proper_positioning(['TEST', '2', '2'], self.toy_robot)
+        is_valid = self.toy_robot.set_initial_robot_position(['TEST', '2', '2'])
         self.assertEqual(is_valid, False)
 
-        is_valid = proper_positioning(['2', '2', '2'], self.toy_robot)
+        is_valid = self.toy_robot.set_initial_robot_position(['2', '2', '2'])
         self.assertEqual(is_valid, False)
 
-        is_valid = proper_positioning(['2', '2', 'EAST'], self.toy_robot)
+        is_valid = self.toy_robot.set_initial_robot_position(['2', '2', 'EAST'])
         self.assertEqual(is_valid, True)
 
-        self.assertEqual(self.toy_robot._position['x'], 2)
-        self.assertEqual(self.toy_robot._position['y'], 2)
-        self.assertEqual(self.toy_robot._position['direction'], 'EAST')
+        self.assertEqual(self.toy_robot.position.x, 2)
+        self.assertEqual(self.toy_robot.position.y, 2)
+        self.assertEqual(self.toy_robot.position.direction, 'EAST')
+
+
+class TestPosition(unittest.TestCase):
+    def setUp(self):
+        self.position = Position()
+
+    def test_is_valid_position(self):
+        is_valid = self.position.is_valid_position('1', '2', '2')
+        self.assertEqual(is_valid, False)
+
+        is_valid = self.position.is_valid_position('1', '1', 'test')
+        self.assertEqual(is_valid, False)
+
+        is_valid = self.position.is_valid_position('1', '1', 'EAST')
+        self.assertEqual(is_valid, True)
 
 
 if __name__ == '__main__':
