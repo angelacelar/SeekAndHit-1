95414d66f9e548687336ed355e19d6b85afc2b05
krunomunitic <krunomunitic@gmail.com>
2018-03-28 15:24:01 +0200

    Lua proper text fix

--- Lua/airplaine_parking.lua
+++ Lua/airplaine_parking.lua
@@ -3,14 +3,12 @@ redis.replicate_commands()
 local plane_id = ARGV[1]
 
 if tonumber(plane_id) == nil then
-  return 'Plane id is not a number.'
+  return 'Plane ID is not a number. Please put correct Plane ID number.'
 end
 
 if tonumber(plane_id) < 1 or tonumber(plane_id) > 81 then
-  return 'Plane id is not in between values of 1 and 80'
+  return 'Plane ID is not in between values of 1 and 80. Please put correct plane ID number.'
 end
-
-local items = redis.call('lrange', 'parked_planes', 0, -1)
 
 if redis.call('hget', 'plane_hash', plane_id) then return tonumber(redis.call('hget', 'plane_hash', plane_id)) end
 
