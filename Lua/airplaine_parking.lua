ac6b589a8481ff603853fb1364e01801791c1669
krunomunitic <krunomunitic@gmail.com>
2018-03-28 15:20:10 +0200

    Lua check if number inbetween

--- Lua/airplaine_parking.lua
+++ Lua/airplaine_parking.lua
@@ -2,6 +2,14 @@ redis.replicate_commands()
 
 local plane_id = ARGV[1]
 
+if tonumber(plane_id) == nil then
+  return 'Plane id is not a number.'
+end
+
+if tonumber(plane_id) < 1 or tonumber(plane_id) > 81 then
+  return 'Plane id is not in between values of 1 and 80'
+end
+
 local items = redis.call('lrange', 'parked_planes', 0, -1)
 
 if redis.call('hget', 'plane_hash', plane_id) then return tonumber(redis.call('hget', 'plane_hash', plane_id)) end
